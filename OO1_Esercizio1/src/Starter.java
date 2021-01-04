e46e31de0f30c6d47f9629c6f34bbeb4a1059e86
TrinchilloGiusiOrnella <76963886+TrinchilloGiusiOrnella@users.noreply.github.com>
2021-01-04 18:21:01 +0100

    Update Starter.java

--- OO1_Esercizio1/src/Starter.java
+++ OO1_Esercizio1/src/Starter.java
@@ -7,12 +7,14 @@ public class Starter {
 		System.out.println("Inserisci la stringa s:");
 		Scanner tastiera = new Scanner(System.in);
 		s=tastiera.nextLine();
-		System.out.println("Il numero totale di caratteri è: " + s.length());
-		int a,b;
-		System.out.println("Inserisci a e b:");
+		System.out.println("Il numero totale di caratteri Ã¨: " + s.length());
+		int a,b,c;
+		System.out.println("Inserisci a e b e c:");
 		a=tastiera.nextInt();
 		b=tastiera.nextInt();
-		System.out.println("La sottostringa è " + s.substring(a-1, b-1));
+		c=tastiera.nextInt();
+		System.out.println("La sottostringa Ã¨ " + s.substring(a-1, b-1));
+		System.out.println("il valore di c Ã¨ " +c);
 		
 		
 		
