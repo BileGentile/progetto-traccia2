020a1bd195cbe661cf1323f2e49effc548126921
BileGentile <76962962+BileGentile@users.noreply.github.com>
2021-01-20 20:26:23 +0100

    Inizio impl Connection e DAO
    Ho iniziato a scrivere la classe DBBuilder per generare la tabella di Membro, ed ho iniziato l'interfaccia MembroDAO

---
+++ SorgenteProgettoTracciaDue/src/dao_impl/MembroDAOPostgresImpl.java
@@ -0,0 +1,29 @@
+package dao_impl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import daos.MembroDAO;
+import entity.Membro;
+
+public class MembroDAOPostgresImpl implements MembroDAO {
+
+	@Override
+	public List<Membro> getAllMembri() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int inserisciMembro(Membro membro) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cancellaMembro(Membro membro) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
