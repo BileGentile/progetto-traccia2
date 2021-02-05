2ec4ff30f7e2cb58df4444fcf7a67899663f7565
SaraEsposito <74970446+SaraEsposito@users.noreply.github.com>
2021-02-05 15:40:11 +0100

    Eseguzione uguale al commit precedente ma con controller aggiornato.

--- SorgenteProgettoTracciaDue/src/app/Controller.java
+++ SorgenteProgettoTracciaDue/src/app/Controller.java
@@ -2,15 +2,20 @@ package app;
 
 import gui.Presentazione;
 import gui.LoginSviluppatore;
+import gui.AggiungiProgetto;
+import gui.BenvenutoProjectManager;
+import gui.BenvenutoSviluppatore;
 import gui.LoginProjectManager;
 
 public class Controller {
 
 	int caso;
 	Presentazione  presenta;
-	LoginProjectManager LoginPM;
-	LoginSviluppatore LoginS;
-	
+	LoginProjectManager loginPM;
+	LoginSviluppatore loginS;
+	BenvenutoProjectManager benvenutoPM;
+	BenvenutoSviluppatore benvenutoS;
+	AggiungiProgetto aggiungiprogetto;
 	public static void main(String[] args) {
 
 		Controller c = new Controller();
@@ -24,25 +29,56 @@ public class Controller {
 
 	public void LoginProjectManager() {
 		presenta.setVisible(false);
-		LoginPM= new LoginProjectManager(this);
-		LoginPM.setVisible(true);
+		loginPM= new LoginProjectManager(this);
+		loginPM.setVisible(true);
 		
 	}
 	public void AvviaLoginSviluppatore() {
 		presenta.setVisible(false);
-		LoginS= new LoginSviluppatore(this);
-		LoginS.setVisible(true);
+		loginS= new LoginSviluppatore(this);
+		loginS.setVisible(true);
 		
 	}
 
 	public void TornaPresentazione(int caso) {
 		if(caso==1) {
-			LoginPM.setVisible(false);
+			loginPM.setVisible(false);
 		}else {
-			LoginS.setVisible(false);
+			loginS.setVisible(false);
 		}
 			
 			presenta=new Presentazione(this);
 			presenta.setVisible(true);
 		}
+
+	public void CreaProgetto() {
+		benvenutoPM.setVisible(false);
+		aggiungiprogetto=new AggiungiProgetto(this);
+		aggiungiprogetto.setVisible(true);
+	}
+
+	public void AvviaBenvenutoPM(String text) {
+		if(text.equals("")) {
+			loginPM.setVisible(false);
+			loginPM.setVisible(true);
+
+		}else {
+			loginPM.setVisible(false);
+			benvenutoPM=new BenvenutoProjectManager (this);
+			benvenutoPM.setVisible(true);
+		}
+	}
+	
+
+	public void AvviaBenvenutoS(String text) {
+		if(text.equals("")) {
+			loginS.setVisible(false);
+			loginS.setVisible(true);
+
+		}else {
+			loginS.setVisible(false);
+			benvenutoS=new BenvenutoSviluppatore (this);
+			benvenutoS.setVisible(true);
+		}
+	}
 }
